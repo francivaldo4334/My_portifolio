7ad604ddf6c0e2f9fe9c8e2fabe3a9fef0602061
francivaldo4334 <frncvldreis@gmail.com>
2022-04-01 13:54:30 -0300

    primary_commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+// const express = require('express');
+// const app = express.express();
+
+
+export default function Home(){
+
+    // document.body.style.backgroundColor = "yellow";
+    // document.getElementById("id").style.backgroundColor = "yellow"
+
+    return <h1>Portifolio</h1>;
+}
