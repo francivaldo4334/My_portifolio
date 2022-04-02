7f0ed87b57eeb2becc4d398bd655d07f42aa06f0
francivaldo4334 <frncvldreis@gmail.com>
2022-04-02 15:11:02 -0300

    primaty layout version

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,39 @@
-// const express = require('express');
-// const app = express.express();
+import styles from '../styles/Home.module.css'
+import Image from 'next/image'
+export default function Home() {
+  return  (
+    <div>
+        <div className={styles.background_img}>
+          <div className={styles.logo}>
+            <img src="./images/logo.png"/>
+            <h1>
+            Mobile Developer<br/>
+            <span>Francivaldo Costa</span>
+            </h1>
+          </div>
+        </div>
+        <div className={styles.div_body}>
+            <div className={styles.img_perfil}>
+               <img src="./images/logo.png"/>
+            </div>
+            <div className={styles.div_bottom}>
+                <h1>Sobre Mim</h1>
+                <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. ...<br/> It is a long established fact that a reader will be distracted by the ...
+                </p>
+                <h1>Contato</h1>
+                <div className={styles.contat_types}>
+                    <img src="./images/ic_phone.png"/>
+                    <p>(86)981266564</p>
+                    <img src="./images/ic_email.png"/>
+                    <p>frncvldreisdev@gmail.com</p>
+                    <img src="./images/"/>
+                    <p>luis_correia,piaui,brasil</p>
+                </div>
+                <h1>Abilidades</h1>
+              </div>
+        </div>
+    </div>
 
-
-export default function Home(){
-
-    // document.body.style.backgroundColor = "yellow";
-    // document.getElementById("id").style.backgroundColor = "yellow"
-
-    return <h1>Portifolio</h1>;
+  )
 }
